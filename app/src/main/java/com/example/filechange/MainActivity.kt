d1ee2732b7ebd6aa5222292ba24b98bbcf049e3b
zhangyumin <1436241312@qq.com>
2020-05-07 10:14:48 +0800

    FileObserver:证实只有app本地目录下文件变化可以被检测

--- app/src/main/java/com/example/filechange/MainActivity.kt
+++ app/src/main/java/com/example/filechange/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.os.Environment
 import android.os.FileObserver
 import android.util.Log
+import android.widget.Button
+import java.io.File
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,8 +17,28 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.i("zym", "PATH: $PATH")
-        mFileChangedObserver = FileChangedObserver(PATH)
+//        Log.i("zym", "PATH: $PATH")
+
+        val file = getDir()
+        mFileChangedObserver = FileChangedObserver(file.path, FileObserver.CREATE)
+
+        val random = Random(10)
+        findViewById<Button>(R.id.create).setOnClickListener {
+            val filePath = file.absolutePath + File.separator + random.nextInt() + ".txt"
+            val newFile = File(filePath)
+            newFile.createNewFile()
+        }
+    }
+
+    private fun getDir(): File {
+        var file = getExternalFilesDir(null)
+        if (file == null) {
+            file = File(filesDir, "FileObserver")
+        }
+        if (!file.exists()) {
+            file.mkdir()
+        }
+        return file
     }
 
     override fun onResume() {
